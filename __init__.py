86481230f1da5cc914cd16e465a8a3262ecacf75
Andrew Meserole <ameserole@gmail.com>
2017-11-16 17:01:49 -0600

    Getting close to finished

--- __init__.py
+++ __init__.py
@@ -3,14 +3,16 @@ from CTFd.plugins.keys import get_key_class
 from CTFd.models import db, Solves, WrongKeys, Keys, Challenges, Files, Tags
 from CTFd import utils
 import sys
+import json
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 class MultiQuestionChallengeModel(Challenges):
-    __mapper_args__ = {'polymorphic_identity': 'MultiQuestionChallenge'}
-    #id = db.Column(None, db.ForeignKey('challenges.id'), primary_key=True)
+    __mapper_args__ = {'polymorphic_identity': 'multiquestionchallenge'}
+    #id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(None, db.ForeignKey('challenges.id'), primary_key=True)
 
-    def __init__(self, name, description, value, category, type='MultiQuestionChallenge'):
+    def __init__(self, name, description, value, category, type='multiquestionchallenge'):
         self.name = name
         self.description = description
         self.value = value
@@ -35,8 +37,8 @@ class MultiQuestionKey(keys.BaseKey):
         return result == 0
 
 class MultiQuestionChallenge(challenges.CTFdStandardChallenge):
-    id = "MultiQuestionChallenge"
-    name = "MultiQuestionChallenge"
+    id = "multiquestionchallenge"
+    name = "multiquestionchallenge"
 
     templates = {  # Handlebars templates used for each aspect of challenge editing & viewing
         'create': '/plugins/CTFd-multi-question-plugin/challenge-assets/multi-challenge-create.hbs',
@@ -67,7 +69,7 @@ class MultiQuestionChallenge(challenges.CTFdStandardChallenge):
             key_type = 'key_type[{}]'.format(i)
             if key_name in request.form:
                 print request.form[key_name]
-                keys[request.form[key_name]] = {'key': request.form[key_sol], 'type': request.form[key_type]} 
+                keys[request.form[key_name]] = {'key': request.form[key_sol], 'type': request.form[key_type]}
             else:
                 break
 
@@ -96,7 +98,9 @@ class MultiQuestionChallenge(challenges.CTFdStandardChallenge):
 
         for key, value in keys.iteritems():
             flag = Keys(chal.id, value['key'], value['type'])
-            flag.data = key
+            flag.data = '{"keyname": "'+key+'", "solved": false}'
+            print flag.data
+            sys.stdout.flush()
             db.session.add(flag)
 
         db.session.commit()
@@ -178,16 +182,21 @@ class MultiQuestionChallenge(challenges.CTFdStandardChallenge):
         :return: (boolean, string)
         """
         provided_key = request.form['key'].strip()
+        provided_keyname = request.form['keyname']
         chal_keys = Keys.query.filter_by(chal=chal.id).all()
+        print "attempt request {}".format(request.form)
+        sys.stdout.flush()
         for chal_key in chal_keys:
-            if get_key_class(chal_key.key_type).compare(chal_key.flag, provided_key):
-                continue
-            else:
-                return False, 'Incorrect'
-        return True, 'Correct'
-
-    def single_solve():
-        pass
+            key_data = json.loads(chal_key.data)
+            if key_data['keyname'] == provided_keyname and get_key_class(chal_key.key_type).compare(chal_key.flag, provided_key):
+                key_data['solved'] = True
+                chal_key.data = str(key_data)
+                db.session.commit()
+                print chal_key.data
+                sys.stdout.flush()
+                return True, 'Correct'
+        
+        return False, 'Incorrect'
 
     @staticmethod
     def solve(team, chal, request):
@@ -199,6 +208,11 @@ class MultiQuestionChallenge(challenges.CTFdStandardChallenge):
         :return:
         """
         provided_key = request.form['key'].strip()
+        chal_keys = Keys.query.filter_by(chal=chal.id).all()
+        for chal_key in chal_keys:
+            if not chal_key.data['solved']:
+                return
+ 
         solve = Solves(teamid=team.id, chalid=chal.id, ip=utils.get_ip(req=request), flag=provided_key)
         db.session.add(solve)
         db.session.commit()
@@ -222,17 +236,92 @@ class MultiQuestionChallenge(challenges.CTFdStandardChallenge):
 
 
 def load(app):
-    challenges.CHALLENGE_CLASSES['MultiQuestionChallenge'] = MultiQuestionChallenge
-    keys.KEY_CLASSES['MultiQuestionKey'] = MultiQuestionKey
+    challenges.CHALLENGE_CLASSES['multiquestionchallenge'] = MultiQuestionChallenge
     register_plugin_assets_directory(app, base_path='/plugins/CTFd-multi-question-plugin/challenge-assets/') 
     register_plugin_assets_directory(app, base_path='/plugins/CTFd-multi-question-plugin/key-assets/')
+    app.db.create_all()
 
     @app.route('/keynames/<int:chalid>')
     def key_names(chalid):
         chal_keys = Keys.query.filter_by(chal=chalid).all()
         key_list = []
         for key in chal_keys:
-            key_list.append(key.data)
+            key_list.append(json.loads(key.data)['keyname'])
 
         return jsonify(key_list)       
+
+    @app.route('/chal/multi/<int:chalid>')
+    def single_solve():
+        if utils.ctf_ended() and not utils.view_after_ctf():
+            abort(403)
+        if not utils.user_can_view_challenges():
+            return redirect(url_for('auth.login', next=request.path))
+        if (utils.authed() and utils.is_verified() and (utils.ctf_started() or utils.view_after_ctf())) or utils.is_admin():
+            team = Teams.query.filter_by(id=session['id']).first()
+            fails = WrongKeys.query.filter_by(teamid=session['id'], chalid=chalid).count()
+            logger = logging.getLogger('keys')
+            data = (time.strftime("%m/%d/%Y %X"), session['username'].encode('utf-8'), request.form['key'].encode('utf-8'), utils.get_kpm(session['id']))
+            print("[{0}] {1} submitted {2} with kpm {3}".format(*data))
+
+            chal = Challenges.query.filter_by(id=chalid).first_or_404()
+            if chal.hidden:
+                abort(404)
+            chal_class = get_chal_class(chal.type)
+
+            # Anti-bruteforce / submitting keys too quickly
+            if utils.get_kpm(session['id']) > 10:
+                if utils.ctftime():
+                    chal_class.fail(team=team, chal=chal, request=request)
+                logger.warn("[{0}] {1} submitted {2} with kpm {3} [TOO FAST]".format(*data))
+                # return '3' # Submitting too fast
+                return jsonify({'status': 3, 'message': "You're submitting keys too fast. Slow down."})
+
+            solves = Solves.query.filter_by(teamid=session['id'], chalid=chalid).first()
+
+            # Challange not solved yet
+            if not solves:
+                provided_key = request.form['key'].strip()
+                saved_keys = Keys.query.filter_by(chal=chal.id).all()
+
+                # Hit max attempts
+                max_tries = chal.max_attempts
+                if max_tries and fails >= max_tries > 0:
+                    return jsonify({
+                        'status': 0,
+                        'message': "You have 0 tries remaining"
+                    })
+
+                status, message = chal_class.attempt(chal, request)
+                if status:  # The challenge plugin says the input is right
+                    if utils.ctftime() or utils.is_admin():
+                        chal_class.solve(team=team, chal=chal, request=request)
+                    logger.info("[{0}] {1} submitted {2} with kpm {3} [CORRECT]".format(*data))
+                    return jsonify({'status': 1, 'message': message})
+                else:  # The challenge plugin says the input is wrong
+                    if utils.ctftime() or utils.is_admin():
+                        chal_class.fail(team=team, chal=chal, request=request)
+                    logger.info("[{0}] {1} submitted {2} with kpm {3} [WRONG]".format(*data))
+                    # return '0' # key was wrong
+                    if max_tries:
+                        attempts_left = max_tries - fails - 1  # Off by one since fails has changed since it was gotten
+                        tries_str = 'tries'
+                        if attempts_left == 1:
+                            tries_str = 'try'
+                        if message[-1] not in '!().;?[]\{\}':  # Add a punctuation mark if there isn't one
+                            message = message + '.'
+                        return jsonify({'status': 0, 'message': '{} You have {} {} remaining.'.format(message, attempts_left, tries_str)})
+                    else:
+                        return jsonify({'status': 0, 'message': message})
+
+            # Challenge already solved
+            else:
+                logger.info("{0} submitted {1} with kpm {2} [ALREADY SOLVED]".format(*data))
+                # return '2' # challenge was already solved
+                return jsonify({'status': 2, 'message': 'You already solved this'})
+        else:
+            return jsonify({
+                'status': -1,
+                'message': "You must be logged in to solve a challenge"
+            })
+
 
--- challenge-assets/multi-challenge-modal.js
+++ challenge-assets/multi-challenge-modal.js
@@ -1,7 +1,8 @@
-$('#submit-key').unbind('click');
-$('#submit-key').click(function (e) {
+$('#submit-key0').unbind('click');
+$('#submit-key0').click(function (e) {
     e.preventDefault();
-    submitkey($('#chal-id').val(), $('#answer-input').val(), $('#nonce').val())
+    console.log(e);
+    submitkey($('#chalid').val(), $('#answer-input0').val(), $('#nonce').val())
 });
 
 $("#answer-input").keyup(function(event){
@@ -28,48 +29,106 @@ var decoded = $('<textarea/>').html(content).val()
 
 $('.chal-desc').html(marked(content, {'gfm':true, 'breaks':true}));
 
+function submitkeynew(chal, key, nonce, count, keyname) {
+    console.log(count);
+
+    $('#submit-key' + count).addClass("disabled-button");
+    $('#submit-key' + count).prop('disabled', true);
+    $.post(script_root + "/chal/" + chal, {
+        keyname: keyname,
+        key: key,
+        nonce: nonce
+    }, function (data) {
+        var result = $.parseJSON(JSON.stringify(data));
+
+        var result_message = $('#result-message' + count);
+        var result_notification = $('#result-notification' + count);
+        var answer_input = $("#answer-input" + count);
+        result_notification.removeClass();
+        result_message.text(result.message);
+
+        if (result.status == -1){
+          window.location = script_root + "/login?next=" + script_root + window.location.pathname + window.location.hash
+          return
+        }
+        else if (result.status == 0){ // Incorrect key
+            result_notification.addClass('alert alert-danger alert-dismissable text-center');
+            result_notification.slideDown();
+
+            answer_input.removeClass("correct");
+            answer_input.addClass("wrong");
+            setTimeout(function () {
+                answer_input.removeClass("wrong");
+            }, 3000);
+        }
+        else if (result.status == 1){ // Challenge Solved
+            result_notification.addClass('alert alert-success alert-dismissable text-center');
+            result_notification.slideDown();
+
+            $('.chal-solves').text((parseInt($('.chal-solves').text().split(" ")[0]) + 1 +  " Solves") );
+
+            answer_input.val("");
+            answer_input.removeClass("wrong");
+            answer_input.addClass("correct");
+        }
+        else if (result.status == 2){ // Challenge already solved
+            result_notification.addClass('alert alert-info alert-dismissable text-center');
+            result_notification.slideDown();
+
+            answer_input.addClass("correct");
+        }
+        else if (result.status == 3){ // Keys per minute too high
+            result_notification.addClass('alert alert-warning alert-dismissable text-center');
+            result_notification.slideDown();
+
+            answer_input.addClass("too-fast");
+            setTimeout(function() {
+                answer_input.removeClass("too-fast");
+            }, 3000);
+        }
+        marksolves();
+        updatesolves();
+        setTimeout(function(){
+          $('.alert').slideUp();
+          $('#submit-key' + count).removeClass("disabled-button");
+          $('#submit-key' + count).prop('disabled', false);
+        }, 3000);
+    })
+}
 
 $.get("/keynames/"+$('#chalid').val(), function(data) {
     console.log(data);
-    var key = `<div class="row submit-row">
-                                    <div class="col-md-9" style="padding-right:0px;padding-left:10px;">
-                                        <span class="input">
-                                            <input class="input-field" type="text" name="answer" id="answer-input" placeholder="Key4" />
-                                        </span>
 
-                                        <input id="chal-id" type="hidden" value="{{id}}">
-                                    </div>
-                                    <div class="col-md-3 key-submit">
-                                        <button type="submit" id="submit-key" tabindex="5" class="btn btn-md btn-theme btn-outlined pull-right" style="height:46.375px">Submit</button>
-                                    </div>
-                                </div>
-                                <div class="row notification-row">
-                                    <div id="result-notification" class="alert alert-dismissable text-center" role="alert" style="display: none;">
-                                      <strong id="result-message"></strong>
-                                    </div>
-                                </div>`
     for(i = 0; i < data.length; i++) {
         key = `
-<div class="row submit-row">
-<div class="col-md-9" style="padding-right:0px;padding-left:10px;">
-<span class="input">
-            <input class="input-field" type="text" name="answer" id="answer-input" placeholder="` + data[i] + `" />
-        </span>
-
-        <input id="chal-id" type="hidden" value="{{id}}">
-    </div>
-    <div class="col-md-3 key-submit">
-        <button type="submit" id="submit-key" tabindex="5" class="btn btn-md btn-theme btn-outlined pull-right" style="height:46.375px">Submit4</button>
-    </div>
-</div>
-<div class="row notification-row">
-    <div id="result-notification" class="alert alert-dismissable text-center" role="alert" style="display: none;">
-      <strong id="result-message"></strong>
-    </div>
-</div>`
+        <div class="row submit-row">
+            <div class="col-md-9" style="padding-right:0px;padding-left:10px;">
+                <span class="input">
+                    <input class="input-field" type="text" name="answer" id="answer-input` + i + `" placeholder="` + data[i] + `" />
+                </span>
+            </div>
+            <div class="col-md-3 key-submit">
+                <button name="` + i + `" type="submit" id="submit-key` + i + `" tabindex="5" class="btn btn-md btn-theme btn-outlined pull-right" style="height:46.375px">Submit</button>
+            </div>
+        </div>
+        <div class="row notification-row">
+            <div id="result-notification` + i + `" class="alert alert-dismissable text-center" role="alert" style="display: none;">
+              <strong id="result-message` + i + `"></strong>
+            </div>
+        </div>`
 
 
         $("#keylist").append(key);
+
+        $('#submit-key' + i).unbind('click');
+        $('#submit-key' + i).click(function (e) {
+            e.preventDefault();
+//            console.log(this.name);
+//            console.log(i);
+            j = this.name;
+            submitkeynew($('#chalid').val(), $('#answer-input' + j).val(), $('#nonce').val(), j, $('#answer-input' + j).placeholder);
+        });
     }
 
 });
+
